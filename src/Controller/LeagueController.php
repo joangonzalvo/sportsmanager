02bea91ca600a24d22638d3ca53a03e915c3fb6d
joangonzalvo <joangocap@gmail.com>
2018-05-18 16:06:52 +0200

    progress with simulation 3.0

--- src/Controller/LeagueController.php
+++ src/Controller/LeagueController.php
@@ -36,8 +36,7 @@ class LeagueController extends Controller
     /**
      * @Route("/league/{thisleague}/simulate",name="simulate")
      */
-    public function simulateLeague($thisleague){
-        $teams = $this->getDoctrine()->getRepository('App:Team')->findAll();
+    public function simulateRound($thisleague){
         $repository = $this->getDoctrine()->getRepository('App:League');
         $actualLeague = $repository->findOneBy(['id' => $thisleague]);
         //GET ALL THE CLASSIFICATIONS OF ALL LEAGUES:
@@ -47,19 +46,27 @@ class LeagueController extends Controller
             ['league' => $actualLeague],
             ['points' => 'DESC']
         );
-        //echo(count($classifications));
-        $comp=0;
+        $classifications2 = $totalclassifications->findBy(
+            ['league' => $actualLeague],
+            ['points' => 'DESC']
+        );
+        /**
+         * FIRST WE SIMULATE WINS, LOST AND DRAWS WITHOUT CARING OF THE POINTS
+         */
+        $count=0;
+        $os = array();//Array for save the id of the used id's
         foreach ($classifications as $local) {
             $localid=$local->getId();
-           foreach ($classifications as $rival) {
+            $os[$count]=$local->getId();
+            $count++;
+           foreach ($classifications2 as $rival) {
                $rivalid=$rival->getId();
                if($localid != $rivalid){
-                   if($rivalid > $comp){
+                   if (in_array($rivalid, $os)){}else{
                        $teamlocal=$local->getTeamId();
                        $teamrival=$rival->getTeamId();
-                       if($teamlocal->getTeamValue() != $teamrival->getTeamValue()){
-                           $dif=$teamlocal->getTeamValue()-$teamrival->getTeamValue();
-                           $dif=$dif+50;
+                       
+                       $dif=($teamlocal->getTeamValue()-$teamrival->getTeamValue())+50;
                            //If diference is more than 100 will be auto 99 dif, to get a "luck" factor. Can only win if rand number is 100
                            if($dif >= 100){
                                $dif=99;                               
@@ -68,11 +75,9 @@ class LeagueController extends Controller
                            //If simulation number is smaller than diference, local team wins. If its the same, its a draw. Else, rival wins.
                            //Win means 3 points and 1 point of value, draw 1 point, lose is 1 point of value less
                            
-                           if($simulation <= $dif){
+                           if($simulation < $dif){
                                //LOCAL TEAM WINS
-                               $localpoints=$local->getPoints()+3;
                                $localwins=$local->getWin()+1;
-                               $local->setPoints($localpoints);
                                $local->setWin($localwins);
                                //Update data
                                $em = $this->getDoctrine()->getManager();
@@ -82,39 +87,46 @@ class LeagueController extends Controller
                                $rivallose=$rival->getLost()+1;
                                $rival->setLost($rivallose);
                                //Update data
-                               $em->persist($rival);
-                               $em->flush();
+                               $bm = $this->getDoctrine()->getManager();
+                               $bm->persist($rival);
+                               $bm->flush();
                                //Now update team value
                                $localvalue=$teamlocal->getTeamValue()+1;
                                $rivalvalue=$teamrival->getTeamValue()-1;
+                               if($rivalvalue<=50){
+                                   $rivalvalue=50;
+                               }
                                $teamlocal->setTeamValue($localvalue);
                                $teamrival->setTeamValue($rivalvalue);
-                               $em->persist($teamlocal);
-                               $em->flush();
-                               $em->persist($teamrival);
-                               $em->flush();
+                               $em2 = $this->getDoctrine()->getManager();
+                               $em2->persist($teamlocal);
+                               $em2->flush();
+                               $bm2 = $this->getDoctrine()->getManager();
+                               $bm2->persist($teamrival);
+                               $bm2->flush();
+                               //echo($teamlocal->getTeamName()." vs ".$teamrival->getTeamName()."LOCAL WINS<br>");
                                
-                           }elseif($simulation == $dif){
+                           }
+                           if($simulation == $dif){
                                //LOCAL DRAW
-                               $localpoints=$local->getPoints()+1;
                                $localdraw=$local->getDraw()+1;
-                               $local->setPoints($localpoints);
                                $local->setDraw($localdraw);
                                //Update data
                                $em = $this->getDoctrine()->getManager();
                                $em->persist($local);
                                $em->flush();
                                //RIVAL DRAW
-                               $rivalpoints=$rival->getPoints()+1;
                                $rivaldraw=$rival->getDraw()+1;
-                               $rival->setPoints($rivalpoints);
                                $rival->setDraw($rivaldraw);
                                //Update data
-                               $em->persist($rival);
-                               $em->flush();
+                               $bm = $this->getDoctrine()->getManager();
+                               $bm->persist($rival);
+                               $bm->flush();
                                //No need of update values
+                               //echo($teamlocal->getTeamName()." vs ".$teamrival->getTeamName()." DRAW <br>");
                                
-                           }else{
+                           }
+                           if($simulation > $dif){
                                //LOCAL LOSE
                                $locallose=$local->getLost()+1;
                                $local->setLost($locallose);
@@ -124,38 +136,73 @@ class LeagueController extends Controller
                                $em->flush();
                                //RIVAL WIN
                                $rivalwin=$rival->getWin()+1;
-                               $rivalpoints=$rival->getPoints()+1;
-                               $rival->setPoints($rivalpoints);
                                $rival->setWin($rivalwin);
                                //Update data
-                               $em->persist($rival);
-                               $em->flush();
+                               $bm = $this->getDoctrine()->getManager();
+                               $bm->persist($rival);
+                               $bm->flush();
                                //Now update team value
                                $localvalue=$teamlocal->getTeamValue()-1;
+                               if($localvalue<=50){
+                                   $localvalue=50;
+                               }
                                $rivalvalue=$teamrival->getTeamValue()+1;
                                $teamlocal->setTeamValue($localvalue);
                                $teamrival->setTeamValue($rivalvalue);
-                               $em->persist($teamlocal);
-                               $em->flush();
-                               $em->persist($teamrival);
-                               $em->flush();
+                               $em2 = $this->getDoctrine()->getManager();
+                               $em2->persist($teamlocal);
+                               $em2->flush();
+                               $bm2 = $this->getDoctrine()->getManager();
+                               $bm2->persist($teamrival);
+                               $bm2->flush();
+                               //echo($teamlocal->getTeamName()." vs ".$teamrival->getTeamName()." RIVAL WIN<br>");
                                
                            }
                            
-                       }
-                       else{//TEAM VALUES ARE THE SAME
-
-                       }
-                   }
+                   }}//!LOCALID!RIVALID
                }
-            }
-            $comp++;
-       }
-        /*$total=count($classifications);
-        $thisTeam = $teams->findOneBy(['id' => 0]);
-        $thisTeamClass = $classifications->findOneBy(['id' => 0]);*/
+        }
+        /**
+         * NOW WE SET THE POINTS FOR EACH TEAM
+         */
+        foreach ($classifications as $thisclas) {
+            $wins=($thisclas->getWin())*3;
+            $draws=$thisclas->getDraw();
+            $total=$wins+$draws;
+            $thisclas->setPoints($total);
+            
+            $rm = $this->getDoctrine()->getManager();
+            $rm->persist($thisclas);
+            $rm->flush();
+        }
         
         
+        //END
+        return $this->showLeague($thisleague);
+    }
+    /**
+     * @Route("/league/{thisleague}/reset",name="reset")
+     */
+    public function resetLeague($thisleague){
+        $repository = $this->getDoctrine()->getRepository('App:League');
+        $actualLeague = $repository->findOneBy(['id' => $thisleague]);
+        //GET ALL THE CLASSIFICATIONS OF ALL LEAGUES:
+        $totalclassifications = $this->getDoctrine()->getRepository('App:Classification');
+        //SEARCH FOR THE CLASSIFICATIONS OF THIS LEAGUE ONLY, ORDERED BY POINTS
+        $classifications = $totalclassifications->findBy(
+            ['league' => $actualLeague],
+            ['points' => 'DESC']
+        );
+        foreach ($classifications as $local) {
+            $local->setPoints(0);
+            $local->setWin(0);
+            $local->setLost(0);
+            $local->setDraw(0);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($local);
+            $em->flush();
+        }
         return $this->showLeague($thisleague);
     }
     
