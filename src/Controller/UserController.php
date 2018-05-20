501b7dc3aab4352be494fe91ff0dfd874b5de4ab
joangonzalvo <joangocap@gmail.com>
2018-05-20 11:38:22 +0200

    Update and delete own user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use App\Entity\User;
 use App\Form\RegisterType;
 
@@ -84,6 +85,100 @@ class UserController extends Controller
             'users' => $users,
             'leagues' => $leagues,
             'name'=> $name]);
+    }
+    /**
+     * @Route("/profile/edituser",name="edituser")
+     */
+    public function editUser(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $leagues = $this->getDoctrine()->getRepository('App:League')->findAll();
+            //this user
+           $user=$this->getUser();
+        
+        //creating the form
+        $form = $this->createForm(RegisterType::class, $user);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encoding password, first we get password in plaintext and then
+    // we encode it.
+            $password=$passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();            
+            return $this->redirectToRoute('profile');
+        }
+        //rendering form
+        return $this->render('user/edituser.html.twig', array(
+            'form' => $form->createView(),
+            'leagues' => $leagues
+        ));
+         
+    }
+    /**
+     * @Route("/profile/deleteuser",name="deleteuser")
+     */
+    public function deleteUser(Request $request, TokenStorageInterface $tokenStorage)
+    {
+            $deleted=0;
+           //this user
+           $user=$this->getUser();
+           //In case the user is a Team owner, will find another member of the team to pass the owner role.
+           if($user->getTeamRole()=="Team_Owner"){
+               $flag=0;
+               
+               $teams = $this->getDoctrine()->getRepository('App:Team')->findAll();
+               $users = $this->getDoctrine()->getRepository('App:User')->findAll();
+               foreach ($teams as $team) {
+                    if($team == $user->getTeamid()){
+                        foreach($users as $thisuser){
+                            if($thisuser->getTeamid()==$team && $thisuser->getTeamRole() != "Team_Owner"){
+                                if($flag==0){
+                                    $flag=1;
+                                    $thisuser->setTeamRole("Team_Owner");
+                                    $entityManager = $this->getDoctrine()->getManager();
+                                    $entityManager->persist($user);
+                                    $entityManager->flush();
+                                }
+                            }
+                        }
+                        if($flag==0){//Means there is no more members in the team to get the owner, team will be auto deleted
+                        //First we need to delete the team from all the classifications
+                        $cs = $this->getDoctrine()->getRepository('App:Classification')->findAll();
+                        foreach($cs as $c){
+                            if($c->getTeam()==$team){
+                                $entityManager = $this->getDoctrine()->getManager();
+                                $entityManager->remove($c);
+                                $entityManager->flush();
+                            }
+                        }
+                        $tokenStorage->setToken(null);
+                        
+                        $entityManager2 = $this->getDoctrine()->getManager();
+                        $entityManager2->remove($user);
+                        $entityManager2->flush();
+                        $deleted=1;
+                            
+                        $entityManager3 = $this->getDoctrine()->getManager();
+                        $entityManager3->remove($team);
+                        $entityManager3->flush();
+                        }
+                    }
+                } 
+           }
+           if($deleted==0){
+               $tokenStorage->setToken(null);
+               
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->remove($user);
+                $entityManager->flush(); 
+           }
+            
+            
+            return $this->redirectToRoute('homeaction');
+         
     }
     
     
