8a33e659fdb9fe8451e86064e05191c0f1479c96
joangonzalvo <joangocap@gmail.com>
2018-05-21 16:49:30 +0200

    Start of adminpanel

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,12 +8,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class AdminController extends Controller
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/adminpanel",name="adminpanel")
      */
-    public function index()
-    {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+    public function admin(){
+        $leagues = $this->getDoctrine()->getRepository('App:League')->findAll();
+        
+        return $this->render('admin/panel.html.twig', array(
+            'leagues' => $leagues
+        ));
     }
-}
+    /**
+     * @Route("/adminpanel/users",name="adminusers")
+     */
+    public function adminUsers(){
+        $users = $this->getDoctrine()->getRepository('App:User')->findAll();
+        $leagues = $this->getDoctrine()->getRepository('App:League')->findAll();
+        $teams = $this->getDoctrine()->getRepository('App:Team')->findAll();
+        
+        return $this->render('admin/users.html.twig',[
+            'users' => $users,
+            'teams' => $teams,
+            'leagues' => $leagues]);
+    }
+    /**
+     * @Route("/adminpanel/teams",name="adminteams")
+     */
+    public function adminTeams(){
+        $users = $this->getDoctrine()->getRepository('App:User')->findAll();
+        $leagues = $this->getDoctrine()->getRepository('App:League')->findAll();
+        $teams = $this->getDoctrine()->getRepository('App:Team')->findAll();
+        
+        return $this->render('admin/teams.html.twig',[
+            'users' => $users,
+            'teams' => $teams,
+            'leagues' => $leagues]);
+    }
+        /**
+     * @Route("/adminpanel/leagues",name="adminleagues")
+     */
+    public function adminLeagues(){
+        $leagues = $this->getDoctrine()->getRepository('App:League')->findAll();
+        $teams = $this->getDoctrine()->getRepository('App:Team')->findAll();
+        
+        return $this->render('admin/leagues.html.twig',[
+            'teams' => $teams,
+            'leagues' => $leagues]);
+    }
+    
+}//ENDCONTROLLER
